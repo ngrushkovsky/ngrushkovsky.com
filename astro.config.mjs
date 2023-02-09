e76c6e5497fc0713a9e21e297078436eed8cc32a
Nikolai Grushkovsky <n@ngrushkovsky.com>
2023-02-10 01:35:13 +0700

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  integrations: [tailwind()],
+  outDir: 'target',
+  srcDir: 'source',
+})
---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  pluginSearchDirs: false,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
