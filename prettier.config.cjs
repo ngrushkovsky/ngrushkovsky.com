92295418116f0d75f533abcf0e95dbe7c3ceea74
Nikolai Grushkovsky <n@ngrushkovsky.com>
2023-04-13 16:42:13 +0700

    Convert Prettier config to JSON

--- prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
-  pluginSearchDirs: false,
-  printWidth: 100,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'all',
-}
