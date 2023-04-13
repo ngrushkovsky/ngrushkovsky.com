0da4f9e531bc47c36668423c648f2ab47b93ca35
Nikolai Grushkovsky <n@ngrushkovsky.com>
2023-04-13 16:44:30 +0700

    Update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
-const colors = require('tailwindcss/colors')
-const defaultConfig = require('tailwindcss/defaultConfig')
+import colors from 'tailwindcss/colors.js'
+import defaultTheme from 'tailwindcss/defaultTheme.js'
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./source/**/*.astro'],
   experimental: { optimizeUniversalDefaults: true },
   plugins: [
-    ({ addBase }) => {
-      addBase({
-        '@font-face': {
-          fontDisplay: 'swap',
-          fontFamily: 'Inter var',
-          fontStyle: 'normal',
-          fontWeight: '100 900',
-          src: 'url("/inter-var.woff2") format("woff2-variations")',
-        },
-        html: {
-          fontVariationSettings: '"opsz" 32',
-        },
-      })
+    {
+      handler: ({ addBase }) => {
+        addBase({
+          '@font-face': {
+            fontDisplay: 'swap',
+            fontFamily: 'Inter var',
+            fontStyle: 'normal',
+            fontWeight: '100 900',
+            src: 'url("/inter-var.woff2") format("woff2-variations")',
+          },
+        })
+      },
     },
   ],
   theme: {
     animation: {
       first: 'fade-in-up 750ms backwards 150ms',
-      second: 'fade-in-up 750ms backwards 300ms',
-      third: 'fade-in-up 750ms backwards 450ms',
-      fourth: 'fade-in-up 750ms backwards 600ms',
+      second: 'fade-in-up 750ms backwards 450ms',
+      third: 'fade-in-up 750ms backwards 750ms',
     },
     colors: {
       black: colors.black,
@@ -34,7 +32,7 @@ module.exports = {
       white: colors.white,
     },
     fontFamily: {
-      sans: 'Inter var, sans-serif',
+      sans: ['Inter var, sans-serif', { fontVariationSettings: '"opsz" 32' }],
     },
     fontSize: ({ theme }) => ({
       base: [
@@ -48,11 +46,11 @@ module.exports = {
     keyframes: {
       'fade-in-up': {
         from: {
-          opacity: 0,
+          opacity: '0',
           transform: 'translateY(1rem)',
         },
         to: {
-          opacity: 1,
+          opacity: '1',
           transform: 'translateY(0)',
         },
       },
@@ -69,7 +67,7 @@ module.exports = {
       lg: '53rem',
     },
     spacing: Object.fromEntries(
-      Object.entries(defaultConfig.theme.spacing).map(([key, value]) => [
+      Object.entries(defaultTheme.spacing).map(([key, value]) => [
         key === 'px' ? key : +key * 4,
         value,
       ]),
